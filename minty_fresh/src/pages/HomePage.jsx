1e37dfd28bd03101ae6f4746f609d811669b61af
savvy8525 <savannahyost85@gmail.com>
2023-10-16 22:40:38 -0400

    setup skeleton page for HomePage

--- minty_fresh/src/pages/HomePage.jsx
+++ minty_fresh/src/pages/HomePage.jsx
@@ -1,9 +1,19 @@
+
 const Homepage = () => {
 
 
     return (
         <div>
-            <p>This is Homepage</p>
+            <h1>Welcome to Empower</h1>
+            <p>A way to tame your imposter syndrome while job hunting</p>
+            <button>Get Started</button>
+            <p>How Empower boosts your confidence</p>
+            <h2>Unleash your best self</h2>
+            <p>We help you brainstorm about your best qualities to create a personal profile</p>
+            <h2>Externalize your imposter</h2>
+            <p>We enable you to personify your insecurities into a gremlin you can tame</p>
+            <h2>Gear up for job interviews</h2>
+            <p>We create personalized interview answers as you shape your profile and gremlin</p>
         </div>
     )
 }
