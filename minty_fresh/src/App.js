fa6690e448c935f79b9d8143cddeb061b4e1c240
savvy8525 <savannahyost85@gmail.com>
2023-10-19 22:05:39 -0400

    added all the file structure for the pages

--- minty_fresh/src/App.js
+++ minty_fresh/src/App.js
@@ -1,7 +1,7 @@
 // import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import Strengths from './pages/Strengths/Strengths';
-import Homepage from './pages/HomePage';
+import Homepage from './pages/HomePage/HomePage';
 import './App.scss';
 
 
