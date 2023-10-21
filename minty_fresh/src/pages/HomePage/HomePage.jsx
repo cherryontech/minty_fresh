5747d4fc357be6bf1b36c594371bd55d46caacdc
savvy8525 <savannahyost85@gmail.com>
2023-10-21 11:45:26 -0400

    added in styles and page elements

--- minty_fresh/src/pages/HomePage/HomePage.jsx
+++ minty_fresh/src/pages/HomePage/HomePage.jsx
@@ -1,19 +1,33 @@
 //MF1-2
+import "./HomePage.scss"
+
+
 const Homepage = () => {
 
+    //TODO: finish button function
+    const nextPageClick = () => {
+        //code to be able to go to next page when button clicked
+    };
 
+    //TODO: clean up styling and add img
     return (
-        <div>
-            <h1>Welcome to Empower</h1>
-            <p>A way to tame your imposter syndrome while job hunting</p>
-            <button>Get Started</button>
-            <p>How Empower boosts your confidence</p>
-            <h2>Unleash your best self</h2>
-            <p>We help you brainstorm about your best qualities to create a personal profile</p>
-            <h2>Externalize your imposter</h2>
-            <p>We enable you to personify your insecurities into a gremlin you can tame</p>
-            <h2>Gear up for job interviews</h2>
-            <p>We create personalized interview answers as you shape your profile and gremlin</p>
+        <div className="homepage">
+            <div className="homepage_get-started-container">
+                {/* <img></img> */}
+                <h1>Welcome to Empower</h1>
+                <p>A way to tame your imposter syndrome while job hunting</p>
+                <button className="get-started-btn" onClick={nextPageClick}>Get Started</button>
+            
+            </div>
+            <div className="homepage_onboarding-container">
+                <p className="empower-yourself">How Empower boosts your confidence</p>
+                <h2>Unleash your best self</h2>
+                <p>We help you brainstorm about your best qualities to create a personal profile</p>
+                <h2>Externalize your imposter</h2>
+                <p>We enable you to personify your insecurities into a gremlin you can tame</p>
+                <h2>Gear up for job interviews</h2>
+                <p>We create personalized interview answers as you shape your profile and gremlin</p>
+            </div>
         </div>
     )
 }
