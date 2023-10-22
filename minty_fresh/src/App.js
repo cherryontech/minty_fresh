57f3a34a7edfabf54cca1b33929139fcb2c88d98
Savannah Yost <savannahyost85@gmail.com>
2023-10-22 13:50:12 -0400

    added content and first round styling

--- minty_fresh/src/App.js
+++ minty_fresh/src/App.js
@@ -3,6 +3,7 @@ import { Route, Routes } from 'react-router-dom';
 import Strengths from './pages/Strengths/Strengths';
 import Homepage from './pages/HomePage/HomePage';
 import './App.scss';
+import GrowthOpportunites from './pages/GrowthOpportunities/GrowthOpportunities';
 
 
 function App() {
@@ -16,6 +17,10 @@ function App() {
         <Route
             path="/strengths"
             element={<Strengths/>}
+          />
+        <Route
+            path="/growth"
+            element={<GrowthOpportunites/>}
           />
       </Routes>
     </div>
