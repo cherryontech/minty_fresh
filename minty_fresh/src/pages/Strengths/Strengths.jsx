aa46e79e6fefb4deb3557df9a9f9ce720efc5467
nathalie_barreto <your_email@github.com>
2023-10-23 14:09:50 -0400

    outline without functionality

--- minty_fresh/src/pages/Strengths/Strengths.jsx
+++ minty_fresh/src/pages/Strengths/Strengths.jsx
@@ -3,6 +3,7 @@ import Header from '../../components/Header/Header';
 
 const Strengths = () => {
 
+
     function handleSubmit(e) {
         // Prevent the browser from reloading the page
         e.preventDefault();
@@ -11,45 +12,63 @@ const Strengths = () => {
         // const formData = new FormData(form);
     }
 
+//   function handleSubmit(e) {
+//     // Prevent the browser from reloading the page
+//     e.preventDefault();
+
+//     // Read the form data
+//     const form = e.target;
+//     const formData = new FormData(form);
+
+//     // You can pass formData as a fetch body directly:
+//     fetch('/some-api', { method: form.method, body: formData });
+
+//     // Or you can work with it as a plain object:
+//     const formJson = Object.fromEntries(formData.entries());
+//     console.log(formJson);
+//   }
+
 
     return (
         <div className='strengths'>
             <section> 
                 <Header/>
             </section>
-            <div className='strengths__header-container n'>
-                <p className="step__text"> Step 2 </p>
-                <h1 className="strengths__title">Let's highlight your strengths & skills</h1>
-            </div>
-            <form method="post" onSubmit={handleSubmit}>
-                <div className="strengths__form">
-
-                <label className='strengths__question'>
-                    What's your most common compliment?
-                    <textarea className='strengths__question-textbox'
-                    name="compliments"
-                    placeholder="Ex. I usually get good compliments about my graphic illustrations and my verbal communication."
-                    />
-                </label>
-                    <label className='strengths__question'>
-                        <p>What activities do you enjoy and have a lot of practice doing?</p>
-                        <textarea className='strengths__question-textbox'
-                        name="activities"
-                        placeholder="Ex. I love to doodle and create graphics for my social media. I enjoy teaching beginners how to paint and I've been doing it for 3 years."
+            <p className="step__text"> Step 2 </p>
+            <h2 className="strengths__title">Let's highlight your strengths & skills</h2>
+            <form className="strengths__form" method="post" onSubmit={handleSubmit}>
+                    <label className="strengths__form-content">
+                        What's your most common compliment? 
+                        <textarea 
+                            type="text"
+                            required
+                            name="compliment" 
+                            placeholder="Ex. I usually get good compliments about my graphic illustrations and my verbal communication."
                         />
                     </label>
-                    <label className='strengths__question'>
+                    <label className="strengths__form-content">
+                        What activities do you enjoy and have a lot of practice doing?
+                        <textarea 
+                            type="text"
+                            required
+                            name="myCheckbox" 
+                            placeholder="Ex. I love to doodle and create graphics for my social media. I enjoy teaching beginners how to paint and I've been doing it for 3 years." 
+                        />
+                    </label>
+                    <label className="strengths__form-content">
                         <p>What tools or methodologies are you most comfortable working with?</p>
-                        <textarea className='strengths__question-textbox'
-                        name="methodologies"
-                        placeholder="Ex. I'm most comfortable with using Figma, Adobe, and Canva. I follow atomic design principles when creating my designs."
-                        />
+                        <textarea 
+                            type="unkown" 
+                            name="Seesoon" 
+                            placeholder="Ex. I'm most comfortable with using Figma, Adobe, and Canva. I follow atomic design principles when creating my designs."
+                            />
                     </label>
-                </div>
-                <button className='button__next'> Next </button>
+                <button className="button__next" type="submit"> Next</button>
             </form>
         </div>
     )
 }
 
 export default Strengths;
+
+
